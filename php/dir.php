cc2296eab436785bdca57f43db209292502d6176
MH <mahui@shopex.cn>
2019-05-22 17:44:30 +0800

    create dirs function

--- php/dir.php
+++ php/dir.php
@@ -1,10 +1,24 @@
 <?php
 
-function mkdirs($path) {
-	if (!is_dir($path)) {
-		mkdirs(dirname($path));
-		mkdir($path);
-	}
+/**
+     * Utility function to recursive directory creation
+     *
+     * @param string $dir
+     * @param integer $mode
+     * @param boolean $recursive
+     * @return boolean
+     */
 
-	return is_dir($path);
-}
+    public static function mkdirs($dir, $mode = 0777, $recursive = true)
+    {
+        if (($dir === null) || $dir === '') {
+            return false;
+        }
+        if (is_dir($dir) || $dir === '/') {
+            return true;
+        }
+        if (self::mkdirs(dirname($dir), $mode, $recursive)) {
+            return mkdir($dir, $mode);
+        }
+        return false;
+    }
